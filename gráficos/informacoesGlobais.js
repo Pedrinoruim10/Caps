604f6905831c4a1656b6be1b0da2c677a37fa85b
Pedro.rlq <128191296+Pedrinoruim10@users.noreply.github.com>
2024-11-04 19:36:55 -0300

    fhtr

--- gráficos/informacoesGlobais.js
+++ gráficos/informacoesGlobais.js
@@ -3,7 +3,10 @@ const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-g
 async function vizualizarInformacoesGlobais() {
     const res = await fetch(url)
     const dados = await res.json()
-    console.log(dados);
+    console.log(dados)
+    const paragrafo = document.createElement('p')
+    paragrafo.classList.add('graficos-conteiner__texto')
+    paragrafo.innerHTML`voce sabia que o mundo ${dados.total_pessoas_mundo} de pessoas que aproximadamente${dados.total_pessoas_conectadas} estao conectadas em redes sociais passam em media ${dados.tempo_medio} horas conectadas.`
 }
 
 vizualizarInformacoesGlobais()
